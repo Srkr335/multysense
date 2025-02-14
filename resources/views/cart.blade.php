3ae18d5a3153f43f3cbbd063bd7cd971f6950ae6
sreejith <srkr335@gmail.com>
2025-02-14 22:29:37 +0530

    updat

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -4,46 +4,46 @@
 @section('content')
 <style>
     .cart-totals td{
-        text-align                                 : right;
+        text-align: right;
     }
     .cart-total th, .cart-total td{
-        color                                      :green;
-        font-weight                                : bold;
-        font-size                                  : 21px !important;
+        color:green;
+        font-weight: bold;
+        font-size: 21px !important;
     }
 </style>
-<main class                                        ="pt-90">
-    <div class                                     ="mb-4 pb-4"></div>
-    <section class                                 ="shop-checkout container">
-        <h2 class                                  ="page-title">Cart</h2>
-        <div class                                 ="checkout-steps">
-            <a href                                ="javascript:void();" class="checkout-steps__item active">
-                <span class                        ="checkout-steps__item-number">01</span>
-                <span class                        ="checkout-steps__item-title">
+<main class="pt-90">
+    <div class="mb-4 pb-4"></div>
+    <section class="shop-checkout container">
+        <h2 class="page-title">Cart</h2>
+        <div class="checkout-steps">
+            <a href="javascript:void();" class="checkout-steps__item active">
+                <span class="checkout-steps__item-number">01</span>
+                <span class="checkout-steps__item-title">
                     <span>Shopping Bag</span>
                     <em>Manage Your Items List</em>
                 </span>
             </a>
-            <a href                                ="javascript:void();" class="checkout-steps__item">
-                <span class                        ="checkout-steps__item-number">02</span>
-                <span class                        ="checkout-steps__item-title">
+            <a href="javascript:void();" class="checkout-steps__item">
+                <span class="checkout-steps__item-number">02</span>
+                <span class="checkout-steps__item-title">
                     <span>Shipping and Checkout</span>
                     <em>Checkout Your Items List</em>
                 </span>
             </a>
-            <a href                                ="javascript:void();" class="checkout-steps__item">
-                <span class                        ="checkout-steps__item-number">03</span>
-                <span class                        ="checkout-steps__item-title">
+            <a href="javascript:void();" class="checkout-steps__item">
+                <span class="checkout-steps__item-number">03</span>
+                <span class="checkout-steps__item-title">
                     <span>Confirmation</span>
                     <em>Order Confirmation</em>
                 </span>
             </a>
         </div>
 
-        <div class                                 ="shopping-cart">
+        <div class="shopping-cart">
             @if($cartItems->count() > 0)
-            <div class                             ="cart-table__wrapper">
-                <table class                       ="cart-table">
+            <div class="cart-table__wrapper">
+                <table class="cart-table">
                     <thead>
                         <tr>
                             <th>Product</th>
@@ -58,49 +58,49 @@
                         @foreach ($cartItems as $cartItem)
                         <tr>
                             <td>
-                                <div class         ="shopping-cart__product-item">
-                                    <img loading   ="lazy" src="{{asset('uploads/products/thumbnails')}}/{{$cartItem->model->image}}" width="120" height="120" alt="" />
+                                <div class="shopping-cart__product-item">
+                                    <img loading="lazy" src="{{asset('uploads/products/thumbnails')}}/{{$cartItem->model->image}}" width="120" height="120" alt="" />
                                 </div>
                             </td>
                             <td>
-                                <div class         ="shopping-cart__product-item__detail">
+                                <div class="shopping-cart__product-item__detail">
                                     <h4>{{$cartItem->name}}</h4>
-                                    <ul class      ="shopping-cart__product-item__options">
-                                        <li>Color  : Yellow</li>
-                                        <li>Size   : L</li>
+                                    <ul class="shopping-cart__product-item__options">
+                                        <li>Color: Yellow</li>
+                                        <li>Size: L</li>
                                     </ul>
                                 </div>
                             </td>
                             <td>
-                                <span class        ="shopping-cart__product-price">${{$cartItem->price}}</span>
+                                <span class="shopping-cart__product-price">${{$cartItem->price}}</span>
                             </td>
                             <td>
-                                <div class         ="qty-control position-relative">
-                                    <input type    ="number" name="quantity" value="{{$cartItem->qty}}" min="1" class="qty-control__number text-center">
-                                    <form method   ="POST" action="{{route('cart.reduce.qty',['rowId'=>$cartItem->rowId])}}">
+                                <div class="qty-control position-relative">
+                                    <input type="number" name="quantity" value="{{$cartItem->qty}}" min="1" class="qty-control__number text-center">
+                                    <form method="POST" action="{{route('cart.reduce.qty',['rowId'=>$cartItem->rowId])}}">
                                         @csrf
                                         @method('PUT')
-                                        <div class ="qty-control__reduce">-</div>
+                                        <div class="qty-control__reduce">-</div>
                                     </form>
-                                    <form method   ="POST" action="{{route('cart.increase.qty',['rowId'=>$cartItem->rowId])}}">
+                                    <form method="POST" action="{{route('cart.increase.qty',['rowId'=>$cartItem->rowId])}}">
                                         @csrf
                                         @method('PUT')
-                                        <div class ="qty-control__increase">+</div>
+                                        <div class="qty-control__increase">+</div>
                                     </form>
                                 </div>
                             </td>
                             <td>
-                                <span class        ="shopping-cart__subtotal">${{$cartItem->subTotal()}}</span>
+                                <span class="shopping-cart__subtotal">${{$cartItem->subTotal()}}</span>
                             </td>
 
                         <td>
-                            <form method           ="POST" action="{{route('cart.item.remove',['rowId'=>$cartItem->rowId])}}">
+                            <form method="POST" action="{{route('cart.item.remove',['rowId'=>$cartItem->rowId])}}">
                                 @csrf
                                 @method("DELETE")
-                                <a href            ="javascript:void(0)" class="remove-cart">
-                                    <svg width     ="10" height="10" viewBox="0 0 10 10" fill="#767676" xmlns="http://www.w3.org/2000/svg">
-                                        <path d    ="M0.259435 8.85506L9.11449 0L10 0.885506L1.14494 9.74056L0.259435 8.85506Z" />
-                                        <path d    ="M0.885506 0.0889838L9.74057 8.94404L8.85506 9.82955L0 0.97449L0.885506 0.0889838Z" />
+                                <a href="javascript:void(0)" class="remove-cart">
+                                    <svg width="10" height="10" viewBox="0 0 10 10" fill="#767676" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M0.259435 8.85506L9.11449 0L10 0.885506L1.14494 9.74056L0.259435 8.85506Z" />
+                                        <path d="M0.885506 0.0889838L9.74057 8.94404L8.85506 9.82955L0 0.97449L0.885506 0.0889838Z" />
                                     </svg>
                                 </a>
                             </form>
@@ -110,22 +110,22 @@
                         @endforeach
                     </tbody>
                 </table>
-                <div class                         ="cart-table-footer">
-                    <input class                   ="form-control" type="text" name="coupon_code" placeholder="Coupon Code">
-                            <input class           ="btn-link fw-medium position-absolute top-0 end-0 h-100 px-4" type="submit" value="APPLY COUPON">
+                <div class="cart-table-footer">
+                    <input class="form-control" type="text" name="coupon_code" placeholder="Coupon Code">
+                            <input class="btn-link fw-medium position-absolute top-0 end-0 h-100 px-4" type="submit" value="APPLY COUPON">
 
-                    <form class                    ="position-relative bg-body" method="POST" action="{{route('cart.empty')}}">
+                    <form class="position-relative bg-body" method="POST" action="{{route('cart.empty')}}">
                         @csrf
                         @method('DELETE')
-                        <button class              ="btn btn-light" type="submit">CLEAR CART</button>
+                        <button class="btn btn-light" type="submit">CLEAR CART</button>
                     </form>
                 </div>
             </div>
-            <div class                             ="shopping-cart__totals-wrapper">
-                <div class                         ="sticky-content">
-                    <div class                     ="shopping-cart__totals">
+            <div class="shopping-cart__totals-wrapper">
+                <div class="sticky-content">
+                    <div class="shopping-cart__totals">
                         <h3>Cart Totals</h3>
-                       <table class                ="cart-totals">
+                       <table class="cart-totals">
                                 <tbody>
                                     <tr>
                                         <th>Subtotal</th>
@@ -133,32 +133,32 @@
                                     </tr>
                                     <tr>
                                         <th>SHIPPING</th>
-                                        <td class  ="text-right">Free</td>
+                                        <td class="text-right">Free</td>
                                     </tr>                             
                                     <tr>
                                         <th>VAT</th>
                                         <td>${{Cart::instance('cart')->tax()}}</td>
                                     </tr>
-                                    <tr class      ="cart-total">
+                                    <tr class="cart-total">
                                         <th>Total</th>
                                         <td>${{Cart::instance('cart')->total()}}</td>
                                     </tr>
                                 </tbody>
                             </table>
                     </div>
-                    <div class                     ="mobile_fixed-btn_wrapper">
-                        <div class                 ="button-wrapper container">
-                            <a href                ="{{ route('cart.checkout') }}" class="btn btn-primary btn-checkout">PROCEED TO CHECKOUT</a>
+                    <div class="mobile_fixed-btn_wrapper">
+                        <div class="button-wrapper container">
+                            <a href="{{ route('cart.checkout') }}" class="btn btn-primary btn-checkout">PROCEED TO CHECKOUT</a>
                         </div>
                     </div>
                 </div>
             </div>
             @else
-                <div class                         ="row">
-                    <div class                     ="col-md-12 text-center pt-5 pb-5">
+                <div class="row">
+                    <div class="col-md-12 text-center pt-5 pb-5">
                         <p>No item found in your cart</p>
 
-                        <a href                    ="{{route('shop.index')}}" class="btn btn-info">Shop Now</a>
+                        <a href="{{route('shop.index')}}" class="btn btn-info">Shop Now</a>
                     </div>
                 </div>
             @endif
@@ -167,7 +167,7 @@
 </main>
 @endsection
 @push("scripts")
-<script src                                        ="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(function() {
         $(".qty-control__increase, .qty-control__reduce").on("click", function() {
